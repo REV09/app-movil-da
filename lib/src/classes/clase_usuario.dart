8268e77c3dd4295fee241cc8210c21b49c2f9506
REV09 <hector.morelos.607@gmail.com>
2022-11-22 18:03:23 -0600

    agregada clase usuario

---
+++ lib/src/classes/clase_usuario.dart
@@ -0,0 +1,55 @@
+class Usuario {
+  late String nombreUsuario;
+  late String nombre;
+  late String apellido;
+  late String correoElectronico;
+  late String contrasena;
+  late int administrador;
+
+  Usuario();
+
+  void setNombreUsuario(String nombreUsuario) {
+    this.nombreUsuario = nombreUsuario;
+  }
+
+  void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  void setCorreoElectronico(String correoElectronico) {
+    this.correoElectronico = correoElectronico;
+  }
+
+  void setContrasena(String contrasena) {
+    this.contrasena = contrasena;
+  }
+
+  void setAdministrador(int administrador) {
+    this.administrador = administrador;
+  }
+
+  String getPermisos() {
+    if (administrador == 1) {
+      return "Si";
+    }
+    return "No";
+  }
+
+  String getNombreUsuario() => nombreUsuario;
+  String getNombre() => nombre;
+  String getApellido() => apellido;
+  String getCorreoElectronico() => correoElectronico;
+  String getContrasena() => contrasena;
+  int getAdministrador() => administrador;
+
+  @override
+  String toString() {
+    return "Nombre de usuario: $nombreUsuario\nNombre: $nombre"
+        "\nApellido: $correoElectronico\nCorreo electronico: $correoElectronico"
+        "\nContraseña $contrasena\nAdministrador: ${getPermisos()}";
+  }
+}
